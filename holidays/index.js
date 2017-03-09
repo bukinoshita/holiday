3818de190bfe0dc66659b8190053c5049e971b91
Vitor Capretz <vitor@idwall.co>
2017-03-09 00:28:00 -0300

    changes how parameters are received

---
+++ holidays/index.js
@@ -0,0 +1,3 @@
+'use strict'
+
+module.exports = {2017: require('./2017')}
--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 'use strict'
 
-const holidays = require('./holidays/2017')
+const holidays = require('./holidays/')
 
-const holiday = (month, day, country = 'us') => {
+const holiday = obj => {
+  const date = new Date()
+  const {year = date.getFullYear(), month, day, country = 'us'} = obj
+
   return new Promise((resolve, reject) => {
+    if (!holidays[year]) {
+      reject('There are no holidays for this year')
+    }
+
     if (month > 12 || day > 31) {
       reject('Invalid date')
     }
 
+    if (!month && !day) {
+      resolve(traverseObj(holidays[year][country], []))
+    }
+
     if (month && day) {
-      if (holidays[country][month]) {
-        resolve(holidays[country][month][day])
+      if (holidays[year][country][month]) {
+        resolve(holidays[year][country][month][day])
       }
 
       reject(false)
     } else if (month) {
-      if (holidays[country][month]) {
-        resolve(holidays[country][month])
+      if (holidays[year][country][month]) {
+        resolve(holidays[year][country][month])
       }
 
-      reject(false)
-    } else if (!month && !day) {
-      if (holidays[country]) {
-        resolve(traverseObj(holidays[country], []))
-      }
       reject(false)
     }
   })
